798d2f2ab7554432b905c0d64a383bece8b2d205
lusardiint <valentumlus@gmail.com>
2026-08-01 21:35:52 +0200

    1.1

---
+++ shares.js
@@ -0,0 +1,10 @@
+// shares.js
+window.vaultShares = {
+    "eljincholagentefuerte": {
+        title: "Hola Jincho!", 
+        message: "Aquí tienes un beat que te quedaria perfecto, es flow 50cent pop", // <-- NUEVO: Mensaje custom
+        files: [
+            { name: "Beat_Grave.wav", type: "audio", path: "@LUSARDI_VT/BEATS/POP/[Bruno Mars, Michael Jackson, Pop Funky Disco] 2Fine 92bpm fmin @lusardiint instagram" },
+        ]
+    }
+};
