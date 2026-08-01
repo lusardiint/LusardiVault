14e2a6982005789ae23816effc067f769304cd65
lusardiint <valentumlus@gmail.com>
2026-08-01 21:40:55 +0200

    Update shares.js

--- shares.js
+++ shares.js
@@ -2,9 +2,14 @@
 window.vaultShares = {
     "eljincholagentefuerte": {
         title: "Hola Jincho!", 
-        message: "Aquí tienes un beat que te quedaria perfecto, es flow 50cent pop", // <-- NUEVO: Mensaje custom
+        message: "Aquí tienes un beat que te quedaria perfecto, es flow 50cent pop",
         files: [
-            { name: "Beat_Grave.wav", type: "audio", path: "@LUSARDI_VT/BEATS/POP/[Bruno Mars, Michael Jackson, Pop Funky Disco] 2Fine 92bpm fmin @lusardiint instagram" },
+            { 
+                name: "Beat_Grave.wav", 
+                type: "audio", 
+                // AÑADIDO: La extensión .wav al final del path (debe ser idéntico al de GitHub)
+                path: "@LUSARDI_VT/BEATS/POP/[Bruno Mars, Michael Jackson, Pop Funky Disco] 2Fine 92bpm fmin @lusardiint instagram.wav" 
+            }
         ]
     }
 };
