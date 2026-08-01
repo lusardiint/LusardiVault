ff842d1a53ec3a827521d2230c371aef11ad6902
lusardiint <valentumlus@gmail.com>
2026-08-01 21:45:05 +0200

    Update shares.js

--- shares.js
+++ shares.js
@@ -8,7 +8,7 @@ window.vaultShares = {
                 name: "Beat_Grave.wav", 
                 type: "audio", 
                 // AÑADIDO: La extensión .wav al final del path (debe ser idéntico al de GitHub)
-                path: "@LUSARDI_VT/BEATS/POP/[Bruno Mars, Michael Jackson, Pop Funky Disco] 2Fine 92bpm fmin @lusardiint instagram.wav" 
+                path: "@LUSARDI_VT/BEATS/POP/[Bruno Mars, Michael Jackson, Pop Funky Disco] 2Fine 92bpm fmin @lusardiint instagram.mp3" 
             }
         ]
     }
