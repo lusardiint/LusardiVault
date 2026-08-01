e2f04f21af9fe9db0ecca224dc2874228662fe93
lusardiint <valentumlus@gmail.com>
2026-08-01 23:03:34 +0200

    Update shares.js

--- shares.js
+++ shares.js
@@ -1,12 +1,12 @@
 window.vaultShares = {
     "eljincholagentefuerte": {
-        "title": "Hola Jincho",
+        "title": "Hola Jincho, soy Lusardi",
         "message": "Ademas del resto de mi trabajo te dejo este instrumental que romperias heavy, flow 50cent o MJ",
-        "accessType": 2, // <--- Importante: 3 entra directo sin pedir correo ni esperar
+        "accessType": 4, // <--- Importante: 3 entra directo sin pedir correo ni esperar
         "files": [
             { 
                 name: "Pa la gente fuerte.mp3", 
-                type: "audio", 
+                type: "audio",
                 path: "Shares/Jincho01_92bpm_fmin.mp3" 
             }
         ]
