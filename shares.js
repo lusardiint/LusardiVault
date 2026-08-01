edc52dd7df07f931bc09257194b885c9d49f041a
lusardiint <valentumlus@gmail.com>
2026-08-01 23:47:14 +0200

    1

--- shares.js
+++ shares.js
@@ -1,4 +1,5 @@
 window.vaultShares = {
+
     "eljincholagentefuerte": {
         "title": "Hola Jincho, soy Lusardi",
         "message": "Ademas del resto de mi trabajo te dejo este instrumental que romperias heavy, flow 50cent o MJ",
@@ -10,5 +11,19 @@ window.vaultShares = {
                 path: "Shares/Jincho01_92bpm_fmin.mp3" 
             }
         ]
+    },
+
+    "lusardi_vt": {
+        "title": "Hi Lusardi",
+        "message": "Como va todo?",
+        "accessType": 3, // <--- Importante: 3 entra directo sin pedir correo ni esperar
+        "files": [
+            { 
+                name: "Beat de muestra.mp3", 
+                type: "audio",
+                path: "Shares/Jincho01_92bpm_fmin.mp3" 
+            }
+        ]
     }
+
 };
